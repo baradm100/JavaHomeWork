0578f1ab66c1b2dad4f57b059d00da21c2cfcdbe
Bar Admoni <bar6020677@gmail.com>
2018-01-18 22:05:08 +0200

    Done Maman15!

---
+++ Maman15/Driver.java
@@ -0,0 +1,13 @@
+public class Driver
+{
+    public static void main(String[] args) {
+        StringList list = new StringList("Baradmoni");
+        //StringList list1 = new StringList("Rotem");
+        //CharNode head = new CharNode('a', 'a', new CharNode('b', 'b', null));
+        //list.setHead(head);
+       // System.out.println(list);
+        //System.out.println(list.length()); // 9
+        //System.out.println(list);
+        System.out.println("".charAt(0));
+    }
+}
